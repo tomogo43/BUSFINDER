5f87948d9ec981f4e81a20d07b296c4a3411aff3
Alexandre <alexandre.condamine@outlook.fr>
2020-11-14 17:04:32 +0100

    CRUD Chauffeurs valide

--- client/busfinder/src/router.js
+++ client/busfinder/src/router.js
@@ -35,6 +35,11 @@ export default new Router({
          path: '/admin',
          name: 'admin',
          component: Admin
-      }
+      },
+      {
+         path: '/chauffeurs',
+         name: 'Liste des Chauffeurs',
+         component: ListeChauffeurs
+      },
    ]
 })
