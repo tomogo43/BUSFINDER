7ce429888c9e72da4802235741cfa5c4b645de28
Thomas ESCUDERO <thomas43hteloire@orange.fr>
2020-11-15 15:02:32 +0100

    suppression code inutile plus ajout du publishing en fonction du service dans channel.js

--- client/busfinder/src/store/index.js
+++ client/busfinder/src/store/index.js
@@ -5,35 +5,13 @@ import app from '../feathers-client';
 
 Vue.use(Vuex)
 
-// const url = "http://localhost:3030/lignes";
-// const headers = { Accept: "application/json" };
 
 export default new Vuex.Store({
   state: {
-    // currentTrajet: {},
     allTrajets: [],
     allChauffeurs: [],
     allLignes: []
   },
-  // mutations: {
-  //   // synchrone
-  //   setTrajet(state, payload) {
-  //     state.allTrajets = payload;
-  //   }
-  // },
-  // actions: {
-  //   // asynchrone
-  //   async setCurrentTrajet(state) {
-  //     const trajet = await fetch(url, { headers});
-  //     console.log(trajet);
-  //     const t = await trajet.json();
-  //     // state.allTrajets = [];
-  //     // t.data.forEach(trajet => {
-  //     state.commit("setTrajet", t.data)
-  //     // })
-
-  //   }
-  // },
   mutations: {
     CREATE_TRAJET: function(state, trajet) {
       state.allTrajets.push(trajet)
--- server/src/channels.js
+++ server/src/channels.js
@@ -9,43 +9,53 @@ module.exports = function(app) {
     app.channel('anonymous').join(connection);
   });
 
-  app.on('login', (authResult, { connection }) => {
-    // connection can be undefined if there is no
-    // real-time connection, e.g. when logging in via REST
-    if(connection) {
-      // Obtain the logged in user from the connection
-      // const user = connection.user;
-      
-      // The connection is no longer anonymous, remove it
-      app.channel('anonymous').leave(connection);
-
-      // Add it to the authenticated user channel
-      app.channel('authenticated').join(connection);
-
-      // Channels can be named anything and joined on any condition 
-      
-      // E.g. to send real-time events only to admins use
-      // if(user.isAdmin) { app.channel('admins').join(connection); }
-
-      // If the user has joined e.g. chat rooms
-      // if(Array.isArray(user.rooms)) user.rooms.forEach(room => app.channel(`rooms/${room.id}`).join(connection));
-      
-      // Easily organize users by email and userid for things like messaging
-      // app.channel(`emails/${user.email}`).join(connection);
-      // app.channel(`userIds/${user.id}`).join(connection);
-    }
+  app.service('chauffeurs').publish((data, hook) => {
+    console.log('Publishing chauffeurs on anonymous', JSON.stringify(data)) // eslint-disable-line
+    return app.channel('anonymous')
   });
 
-  // eslint-disable-next-line no-unused-vars
-  app.publish((data, hook) => {
-    // Here you can add event publishers to channels set up in `channels.js`
-    // To publish only for a specific event use `app.publish(eventname, () => {})`
-
-    console.log('Publishing all events to all authenticated users. See `channels.js` and https://docs.feathersjs.com/api/channels.html for more information.'); // eslint-disable-line
-
-    // e.g. to publish all service events to all authenticated users use
-    return app.channel('authenticated');
+  app.service('lignes').publish((data, hook) => {
+    console.log('Publishing lignes on anonymous', JSON.stringify(data)) // eslint-disable-line
+    return app.channel('anonymous')
   });
+
+  // app.on('login', (authResult, { connection }) => {
+  //   // connection can be undefined if there is no
+  //   // real-time connection, e.g. when logging in via REST
+  //   if(connection) {
+  //     // Obtain the logged in user from the connection
+  //     // const user = connection.user;
+      
+  //     // The connection is no longer anonymous, remove it
+  //     app.channel('anonymous').leave(connection);
+
+  //     // Add it to the authenticated user channel
+  //     app.channel('authenticated').join(connection);
+
+  //     // Channels can be named anything and joined on any condition 
+      
+  //     // E.g. to send real-time events only to admins use
+  //     // if(user.isAdmin) { app.channel('admins').join(connection); }
+
+  //     // If the user has joined e.g. chat rooms
+  //     // if(Array.isArray(user.rooms)) user.rooms.forEach(room => app.channel(`rooms/${room.id}`).join(connection));
+      
+  //     // Easily organize users by email and userid for things like messaging
+  //     // app.channel(`emails/${user.email}`).join(connection);
+  //     // app.channel(`userIds/${user.id}`).join(connection);
+  //   }
+  // });
+
+  // // eslint-disable-next-line no-unused-vars
+  // app.publish((data, hook) => {
+  //   // Here you can add event publishers to channels set up in `channels.js`
+  //   // To publish only for a specific event use `app.publish(eventname, () => {})`
+
+  //   console.log('Publishing all events to all authenticated users. See `channels.js` and https://docs.feathersjs.com/api/channels.html for more information.'); // eslint-disable-line
+
+  //   // e.g. to publish all service events to all authenticated users use
+  //   return app.channel('authenticated');
+  // });
 
   // Here you can also add service specific event publishers
   // e.g. the publish the `users` service `created` event to the `admins` channel
